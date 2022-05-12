ce9f8bcd2b3c68f5cb4503c4c54ba13ac4222c84
denis <denismatysik@gmail.com>
2022-05-12 13:46:43 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+const filmsCard = {
+    1: {
+        name: "americanPie",
+        way: "./imgs/1.png"
+    },
+    2: {
+        name: "batman",
+        way: "./imgs/2.png"
+    },
+    3: {
+        name: "dateMovie",
+        way: "./imgs/3.png"
+    },
+    4: {
+        name: "guardians",
+        way: "./imgs/4.png"
+    },
+    5: {
+        name: "thor",
+        way: "./imgs/5.png"
+    },
+    6: {
+        name: "potter",
+        way: "./imgs/6.png"
+    },
+    7: {
+        name: "pirates",
+        way: "./imgs/7.png"
+    },
+    8: {
+        name: "lordOfRings",
+        way: "./imgs/8.png"
+    },
+}
+
+const backSide = "./imgs/back.png"
+
+const myGame = document.querySelector(".memory-game");
+
+for( film in  filmsCard){
+    const memoryCard = document.createElement("div");
+    memoryCard.classList.add("memory-card");
+    memoryCard.setAttribute("data-film", filmsCard[film].name)
+    const img1 = document.createElement("img");
+    const img2 = document.createElement("img");
+    img1.classList.add("front-face");
+    img2.classList.add("back-face");
+    img1.setAttribute("src", filmsCard[film].way)
+    img2.setAttribute("src", backSide)
+    memoryCard.appendChild(img1);
+    memoryCard.appendChild(img2);
+    myGame.appendChild(memoryCard);
+    const memoryCard2 = memoryCard.cloneNode(true);
+    memoryCard.after(memoryCard2);
+}
+
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlipped = false;
